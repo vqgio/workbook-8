250c6c7041a224eae728fb4d24cf454fde1327ac
Gio <giovannizavala82@gmail.com>
2025-06-09 15:42:15 -0500

    Exercise 2. Not completed...will spend more time with prepStatements

--- NorthwindTraders/src/main/java/Main.java
+++ NorthwindTraders/src/main/java/Main.java
@@ -6,14 +6,27 @@ public class Main {
             String username = "root";
             String password = "yearup";
             String url = "jdbc:mysql://localhost:3306/northwind";
-            String query = "SELECT productName FROM products;";
+//            String secureQuery = "SELECT * FROM FILM WHERE title = ?";
+//            String secureQuery = "SELECT * FROM FILM WHERE ? or film_id = ?";
+            String query = "SELECT productName, ProductID, UnitPrice, UnitsInStock FROM products;"; //WHERE = ?
 
             try (Connection connection = DriverManager.getConnection(url, username, password)) {
                 Statement statement = connection.createStatement();
                 ResultSet results = statement.executeQuery(query);
+//                PreparedStatement preparedStatement = connection.prepareStatement(SecureQuery);
+//                PreparedStatement.setString(1, userInput);
+//                PreparedStatement.setInt(2, filmId);
+//                ResultSet rs = PreparedStatement.executeQuery();
                 while (results.next()) {
                     String productName = results.getString("ProductName");
+                    String productId = results.getString("ProductID");
+                    String unitInStock = results.getString("UnitsInStock");
+                    String unitPrice = results.getString("UnitPrice");
                     System.out.println(productName);
+                    System.out.println(productId);
+                    System.out.println(unitPrice);
+                    System.out.println(unitInStock);
+//                    System.out.println("Title: " + rs.getString("title"));
                 }
             }
     }
