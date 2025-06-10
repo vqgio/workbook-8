db37d700f0580a1c61d3fa42ddce1e4e68f4aa38
Gio <giovannizavala82@gmail.com>
2025-06-10 13:20:29 -0500

    Exercise 3 Completed.

--- NorthwindTraders/src/main/java/Main.java
+++ NorthwindTraders/src/main/java/Main.java
@@ -29,32 +29,6 @@ public class Main {
                     System.out.println("pick only from the following options, please try again.");
             }
         }
-
-
-//            String secureQuery = "SELECT * FROM FILM WHERE title = ?";
-//            String secureQuery = "SELECT * FROM FILM WHERE ? or film_id = ?";
-//        String query = "SELECT productName, ProductID, UnitPrice, UnitsInStock FROM products;"; //WHERE = ?
-//
-//
-//        try (Connection connection = DriverManager.getConnection(url, username, password)) {
-//            Statement statement = connection.createStatement();
-//            ResultSet results = statement.executeQuery(query);
-////                PreparedStatement preparedStatement = connection.prepareStatement(SecureQuery);
-////                PreparedStatement.setString(1, userInput);
-////                PreparedStatement.setInt(2, filmId);
-////                ResultSet rs = PreparedStatement.executeQuery();
-//            while (results.next()) {
-//                String productName = results.getString("ProductName");
-//                String productId = results.getString("ProductID");
-//                String unitInStock = results.getString("UnitsInStock");
-//                String unitPrice = results.getString("UnitPrice");
-//                System.out.println(productName);
-//                System.out.println(productId);
-//                System.out.println(unitPrice);
-//                System.out.println(unitInStock);
-////                    System.out.println("Title: " + rs.getString("title"));
-//            }
-//        }
     }
 
     private static void displayAllCustomers() throws ClassNotFoundException {
