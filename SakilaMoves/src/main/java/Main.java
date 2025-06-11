2ff56fe73b63e6ce5264ca005eb0cab56980dce7
Gio <giovannizavala82@gmail.com>
2025-06-11 14:40:16 -0500

    re-arranged some code in the main class and got search by last name working.

--- SakilaMoves/src/main/java/Main.java
+++ SakilaMoves/src/main/java/Main.java
@@ -1,22 +1,69 @@
 import org.apache.commons.dbcp2.BasicDataSource;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Main {
-    private static Scanner scanner = new Scanner(System.in);
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final String url = "jdbc:mysql://localhost:3306/sakila?user=root&password=yearup";
+
     public static void main(String[] args) {
         System.out.println("Good day!");
-        System.out.println("what is the last name of your favorite actor?: ");
-        String userInput = scanner.nextLine().trim();
-        String url = "jdbc:mysql://localhost:3306/sakila?user=root&password=yearup";
+        boolean keepGoing = true;
 
         try(BasicDataSource dataSource = new BasicDataSource()) {
             dataSource.setUrl(url);
             Connection connection = dataSource.getConnection();
+
+            while (keepGoing) {
+                System.out.println("What would you like to do today?");
+                System.out.println("1) Search actor by last name");
+                System.out.println("2) Search actor by full name to find movies in");
+                System.out.println("0) Exit");
+                System.out.print("Choose an option: ");
+                int userInput = Integer.parseInt(scanner.nextLine());
+                System.out.println("====================================");
+                switch(userInput) {
+                    case 1 -> searchByLastName(connection);
+                    case 2 -> searchByFullName(connection);
+                    case 0 -> {
+                        keepGoing = false;
+                        System.out.println("Goodbye!....");
+                    }
+                    default -> System.out.println("Please choose a valid option!");
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("Connection error!!!");
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void searchByLastName(Connection connection) {
+        System.out.print("Enter actors last name: ");
+        String lastName = scanner.nextLine();
+        System.out.println("====================================");
+
+        String secureQuery = "SELECT actor_id, first_name, last_name FROM actor WHERE last_name = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(secureQuery)) {
+            preparedStatement.setString(1, lastName);
+            ResultSet results = preparedStatement.executeQuery();
+
+            while (results.next()) {
+                System.out.println("===== Results =====");
+                System.out.println("Actor ID: " + results.getInt("actor_id"));
+                System.out.println("First Name: " + results.getString("first_name"));
+                System.out.println("Last Name: " + results.getString("last_name"));
+                System.out.println("====================================");
+            }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+    private static void searchByFullName(Connection connection) {
+
     }
 }
