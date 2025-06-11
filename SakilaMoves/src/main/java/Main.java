38a6f13ceae188d7570242325487f95e0999a5bc
Gio <giovannizavala82@gmail.com>
2025-06-11 15:01:23 -0500

    got search by full name working...

--- SakilaMoves/src/main/java/Main.java
+++ SakilaMoves/src/main/java/Main.java
@@ -51,19 +51,47 @@ public class Main {
         try (PreparedStatement preparedStatement = connection.prepareStatement(secureQuery)) {
             preparedStatement.setString(1, lastName);
             ResultSet results = preparedStatement.executeQuery();
-
+            //can be better with if do else statement
             while (results.next()) {
                 System.out.println("===== Results =====");
                 System.out.println("Actor ID: " + results.getInt("actor_id"));
                 System.out.println("First Name: " + results.getString("first_name"));
                 System.out.println("Last Name: " + results.getString("last_name"));
-                System.out.println("====================================");
+                System.out.println("------------------------------------");
             }
+            System.out.println("====================================");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
     private static void searchByFullName(Connection connection) {
+        System.out.println("Enter actor first name: ");
+        String firstName = scanner.nextLine();
+        System.out.println("Enter actor last name: ");
+        String lastName = scanner.nextLine();
+        String secureQuery = """
+            SELECT f.title
+            FROM film f
+            JOIN film_actor fa ON f.film_id = fa.film_id
+            JOIN actor a ON fa.actor_id = a.actor_id
+            WHERE a.first_name = ? AND a.last_name = ?
+        """;
+        try (PreparedStatement preparedStatement = connection.prepareStatement(secureQuery)) {
+            preparedStatement.setString(1, firstName);
+            preparedStatement.setString(2, lastName);
+            ResultSet results = preparedStatement.executeQuery();
+
+            if (results.next()) {
+                System.out.println("Movies featuring: " + firstName + " " + lastName);
+                do {
+                    System.out.println("Movie title: " + results.getString("title"));
+                } while (results.next());
+            } else {
+                System.out.println("No films where found where actor " + firstName + " " + lastName + " features in.");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
